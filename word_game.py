9c8fc5a6e3f341fac2e9e5a00a8159cc62fbf0ed
Christopher Snell <chris.snell2019@gmail.com>
2021-10-25 15:33:24 -0700

    Changed scoring

--- word_game.py
+++ word_game.py
@@ -3,7 +3,7 @@ player_1 = input("Please enter a word: ").lower()
 player_2 = input("Please enter a word: ").lower()
 
 #Establishing the scoring for each word
-scores = [3, 2, 1, 4, 2, 2, 3, 1, 1, 3, 4, 2, 1, 3, 1, 2, 3, 2, 2, 1, 1, 3, 2, 2, 3, 5]
+scores = [3, 2, 1, 3, 2, 2, 3, 1, 1, 3, 2, 2, 1, 3, 1, 2, 3, 2, 2, 1, 1, 3, 2, 2, 3, 3]
 letters = [
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
             'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
