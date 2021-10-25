221e586eb234b4843f3bc34834fb5c34c6d61e24
Christopher Snell <chris.snell2019@gmail.com>
2021-10-25 15:15:31 -0700

    Added commits and created players' score variable.

--- word_game.py
+++ word_game.py
@@ -1,9 +1,13 @@
+# Creating player input for word
 player_1 = input("Please enter a word: ")
 player_2 = input("Please enter a word: ")
 
+#Establishing the scoring for each word
 scores = [3, 2, 1, 4, 2, 2, 3, 1, 1, 3, 4, 2, 1, 3, 1, 2, 3, 2, 2, 1, 1, 3, 2, 2, 3, 5]
 letters = [
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
             'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-            
+# Creating variables to store the score for each player
+score_1 = 0
+score_2 = 0
